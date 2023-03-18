f9d98fbe6cd81a9d438447c06598016f62cb631b
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-03-18 04:48:07 -0400

    Add notification service.

---
+++ NotificationService/Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.Models
+{
+    public class Notification
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string EmailTo { get; set; }
+        public string MessageBody { get; set; }
+    }
+}
--- NotificationService/NotificacionFunction.cs
+++ NotificationService/NotificacionFunction.cs
@@ -1,16 +1,33 @@
 using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using NotificationService.Models;
 
 namespace NotificationService
 {
     public class NotificacionFunction
     {
         [FunctionName("Notification")]
-        public void Run([ServiceBusTrigger("myqueue", Connection = "")]string myQueueItem, ILogger log)
+        public async Task RunAsync(
+            [ServiceBusTrigger("queue", Connection = "ServiceBusConnection")] string messageItem,
+            [CosmosDB(Connection = "CosmosDBConnection")] CosmosClient client,
+            ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            var message = JsonConvert.DeserializeObject<Message>(messageItem);
+            var notification = new Notification
+            {
+                Id = Guid.NewGuid(),
+                DateTime = DateTime.Now,
+                EmailTo = message.Email,
+                MessageBody = $"Transaction alert: ${message.Amount} {message.Type} made to/from your account",
+            };
+
+            var container = client.GetDatabase("NotificationDb").GetContainer("Notifications");
+            await container.CreateItemAsync(notification);
         }
     }
 }
